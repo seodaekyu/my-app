e902eb5f99651ea0939b0f152f07a1c65ad65d29
seodaekyu <grarlove7@naver.com>
2024-02-29 02:20:26 +0900

    리액트 입문

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes, Route, Link} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Counter from "./pages/Counter";
+import Input from "./pages/Input";
+import Input2 from "./pages/Input2";
+import List from "./pages/List";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/About">About</Link> |{" "}
+        <Link to="/Counter">Counter</Link> | <Link to="/Input">Input</Link> |{" "}
+        <Link to="/Input2">Input2</Link> |{" "}
+        <Link to="/List">List</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/counter" element={<Counter />} />
+        <Route path="/input" element={<Input />} />
+        <Route path="/input2" element={<Input2 />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Home = () => {
+    return <h1>Home 화면 입니다.</h1>
+}
+
+export default Home;
+
+// import React from "react";
+
+// function Home () {
+//     return <h1>Home 화면 입니다.</h1>
+// }
+
+// export default Home;
+
+
+// import React, {Component} from "react";
+
+// class Home extends Component {
+//     render() {
+//         return <h1>Home 화면 입니다.</h1>
+//     }
+// }
+
+// export default Home;
